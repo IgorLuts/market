bc6ca3f5dda13edb24d60c0bff9ded0aa2fe7f1c
alexxty7 <alexxty7@gmail.com>
2015-12-11 09:33:50 +0200

    add article styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,10 @@
-product_image = Product.first.image
-product_gallery = Product.first.gallery
+Article.destroy_all
 30.times do |index|
-  Product.create!({
+  Article.create!({
     title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Number.decimal(2),
-    image: product_image,
-    gallery: product_gallery,
-    categories: [Category.first],
-    tags: [Tag.first]
+    content: Faker::Lorem.paragraph,
+    description: Faker::Lorem.paragraph(10)
 })
 end
 
-
-p "Created #{Product.count} products"
+p "Created #{Article.count} Articles"
