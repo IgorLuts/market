5822e6f78237cec80315ea45d2a536ecdfc831c5
alexxty7 <alexxty7@gmail.com>
2016-07-23 12:50:25 +0300

    refactor category children products finder

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.includes(:products).friendly.find(params[:id])
-    @products = @category.products.page(params[:page])
+    @products = @category.products_with_children.page(params[:page])
     if request.path != category_path(@category)
       redirect_to category_path(@category), status: :moved_permanently
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,12 +2,17 @@ class Category < ActiveRecord::Base
   extend FriendlyId
 
   has_many :products
-  
+
   has_ancestry :orphan_strategy => :rootify, :cache_depth => true
 
   friendly_id :name, use: :slugged
 
   def normalize_friendly_id(text)
     text.to_slug.normalize(transliterations: :russian).to_s
+  end
+
+  def products_with_children
+    return products unless has_children?
+    children.inject(products) { |a, e| a << e.products }
   end
 end
