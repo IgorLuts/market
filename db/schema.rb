551901dd4f3bc4e927a13afd71f493c190e5623f
alexxty7 <alexxty7@gmail.com>
2016-07-24 16:54:35 +0300

    add commetn to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716164614) do
+ActiveRecord::Schema.define(version: 20160724134751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20160716164614) do
     t.float    "total_price"
     t.string   "phone"
     t.integer  "user_id"
+    t.string   "comment"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
