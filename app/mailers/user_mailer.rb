cac63dba65b362fa3e9c8161f5524bf0b305ad14
Alexandr <alexxty7@gmail.com>
2017-06-09 22:27:02 +0300

    Update contacts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,21 +1,21 @@
 class UserMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
-    mail(to: ['alexxty7@gmail.com', 'i.luts.aot@gmail.com'], subject: 'Website Contact')
+    mail(to: ['i.luts.aot@gmail.com'], subject: 'Website Contact')
   end
 
   def order_notification(order)
     @order = order
-    mail(to: ['alexxty7@gmail.com', 'i.luts.aot@gmail.com'], subject: 'Order received')
+    mail(to: ['i.luts.aot@gmail.com'], subject: 'Order received')
   end
 
   def callback(contact)
     @contact = contact
-    mail(to: ['alexxty7@gmail.com', 'i.luts.aot@gmail.com'], subject: 'Callback')
+    mail(to: ['i.luts.aot@gmail.com'], subject: 'Callback')
   end
 
   def comment_notification(comment)
     @comment = comment
-    mail(to: ['alexxty7@gmail.com', 'i.luts.aot@gmail.com'], subject: 'Comment received')
+    mail(to: ['i.luts.aot@gmail.com'], subject: 'Comment received')
   end
 end
